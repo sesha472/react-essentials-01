7e77f7e6d536d95942f24e0435709072e05a5679
seshasai <sesha472@gmail.com>
2024-03-12 13:21:00 +0530

    practice esential topics of react from scract

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,70 @@
+
+  import reactImg from './assets/react-core-concepts.png';
+  // import reactIMg2 from './assets/components.png';
+  import { CORE_CONCEPTS } from './data';
+
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core','importent'];
+function genrateRandomnumbers(max){
+  let num = Math.floor(Math.random ()*(max));
+  return num;
+}
+ 
+ const Header = () => {
+  const dynamicgenaraatoe=reactDescriptions[genrateRandomnumbers(reactDescriptions.length)]
+   return (
+    <header>
+        <img src={reactImg} alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          {dynamicgenaraatoe} React concepts you will need for almost any app you are
+          going to build!
+        </p>
+                   
+      </header>
+   )
+ }
+ 
+
+ function Coreconcepts(props){
+  return(
+    <li>
+    <img src={props.img} alt={props.title}></img>
+    <h1>{props.title}</h1>
+    <p>{props.discrption}</p>
+    </li>
+    )
+ }
+ 
+ 
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header/>
       <main>
-        <h2>Time to get started!</h2>
+       <section id='core-concepts'>
+        <h2> Core concepts </h2>
+        <ul>
+          <Coreconcepts title={CORE_CONCEPTS[0].title} 
+          discrption={CORE_CONCEPTS[0].description}
+          img={CORE_CONCEPTS[0].image}/>
+
+          <Coreconcepts title={CORE_CONCEPTS[1].title} 
+          discrption={CORE_CONCEPTS[1].description}
+          img={CORE_CONCEPTS[1].image}/>
+
+          <Coreconcepts title={CORE_CONCEPTS[2].title} 
+          discrption={CORE_CONCEPTS[2].description}
+          img={CORE_CONCEPTS[3].image}/>
+          
+          <Coreconcepts title={CORE_CONCEPTS[3].title} 
+          discrption={CORE_CONCEPTS[3].description}
+          img={CORE_CONCEPTS[3].image}/>
+          
+          
+        </ul>
+
+       </section>
       </main>
     </div>
   );
