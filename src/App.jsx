fd07735fdeb79ede6319fe33f45aa42f79951d57
seshasai <sesha472@gmail.com>
2024-03-21 17:05:45 +0530

    outputing data dynamically using map and practicening img concepts

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,7 @@ function App() {
           </ul>
           
           
+          
         
 
        </section>
--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import React from "react";
 
-const entryPoint = document.getElementById("root");
+const entryPoint = document.getElementById("root"); 
 ReactDOM.createRoot(entryPoint).render(React.createElement(App));
 
 //we can create elemnts without jsx ,withot jsx we dont need build procedure ,but its convenient and way to use jsx in our projects
