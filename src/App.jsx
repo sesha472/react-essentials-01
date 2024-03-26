182a7a34f1271f2c3150de2781f46361caf92369
seshasai <sesha472@gmail.com>
2024-03-26 15:35:15 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Coreconcepts from "./Components/CoreConcepts";
+// import Coreconcepts from "./Components/CoreConcepts";
+
 import Header from "./Components/Headercomponent/Header";
 import TabButton from "./Components/TabButton";
 // import reactImg from './assets/react-core-concepts.png';
 // import reactIMg2 from './assets/components.png';
-import { CORE_CONCEPTS } from "./data";
-import { Fragment, useState } from "react";
+
 
 import CoreConcept from "./Components/CoreConcept";
 import Examples from "./Components/Examples";
